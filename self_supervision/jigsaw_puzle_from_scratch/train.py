050886472912708f8d407e02aa66fa8c3c9371fc
DarioRugg <56448497+DarioneNazionale@users.noreply.github.com>
2021-08-23 12:27:15 +0200

    adding hydra configs

--- self_supervision/jigsaw_puzle_from_scratch/train.py
+++ self_supervision/jigsaw_puzle_from_scratch/train.py
@@ -8,28 +8,30 @@ from dataloaders.lightning_dataloader import SelfSupervisionDataModule
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
 
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
-args = {
-    "lr": 1e-4,
-    "epochs": 30
-}
 
-def main():
+@hydra.main(config_path="conf", config_name="config")
+def main(cfg: DictConfig):
+    print("Config:\n", OmegaConf.to_yaml(cfg))
+
     data_path = join("..", "..", "data", "CUHK-SYSU", "Image", "SSM")
+    checkpoint_path = join("checkpoints")
     dataset = SelfSupervisionDataModule(data_path)
 
     # Network initialize
-    model = PretrainedNet(lr = args["lr"])
+    model = PretrainedNet(lr=cfg.model.lr)
 
     # Train the Model
-    logger = TensorBoardLogger("tb_logs", name="test")
-    trainer = Trainer(gpus=1 if torch.cuda.is_available() else 0, max_epochs=args["epochs"], logger=logger)
+    logger = TensorBoardLogger("tb_logs", name=cfg.sim_name)
+    trainer = Trainer(gpus=1 if torch.cuda.is_available() else 0, max_epochs=cfg.model.epochs, logger=logger)
 
     trainer.fit(model, datamodule=dataset)
 
-    if not os.path.exists("checkpoints"):
-        os.makedirs("checkpoints")
-    model.save(join("checkpoints", "test_checkpoint"))
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+    model.save(join(checkpoint_path, cfg.sim_name+"_checkpoint"))
 
 
 if __name__ == "__main__":
